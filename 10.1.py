f15be64aaef4bb59a9405a278ad152fcbf476de9
ecease <venderdropp@gmail.com>
2025-02-15 11:21:25 +0300

    10.1.py

--- 10.1.py
+++ 10.1.py
@@ -7,38 +7,38 @@
 
 
 
-# Ввод размера матрицы
+#Размера матрицы
 M = int(input("Введите размер матрицы M: "))
 
-# Считываем матрицу
+#Тут типо пишет 1 2 3 
 matrix = []
 print("Введите матрицу построчно:")
 for _ in range(M):
     row = list(map(int, input().split()))
     matrix.append(row)
 
-# Поворот на 90° по часовой стрелке
-matrix_90 = [[0]*M for _ in range(M)]
-for i in range(M):
-    for j in range(M):
-        matrix_90[j][M-1-i] = matrix[i][j]
+# Поворот на 90
+matrix_90 = [[0]*M for _ in range(M)] # делаяется пустая матрица
+for i in range(M): # тут читаесят наша матрица
+    for j in range(M): # проходим по столбцам исходной матрицы
+        matrix_90[j][M-1-i] = matrix[i][j] #перенос элементов чтобы сделать перевернутую матрицу
 
-# Поворот на 180° по часовой стрелке
+# Поворот на 180
 matrix_180 = [[0]*M for _ in range(M)]
 for i in range(M):
     for j in range(M):
         matrix_180[M-1-i][M-1-j] = matrix[i][j]
 
-# Поворот на 270° по часовой стрелке
+# Поворот на 270
 matrix_270 = [[0]*M for _ in range(M)]
 for i in range(M):
     for j in range(M):
         matrix_270[M-1-j][i] = matrix[i][j]
 
-# Вывод результатов
+# Результат
 print("\nИсходная матрица:")
 for row in matrix:
-    print(*row)
+    print(*row) #вывод через пробел
 
 print("\nПоворот на 90°:")
 for row in matrix_90:
